c1872e7952dc98b684744107f772bf34ea64418b
Eduardo Gamboa <69367792+Eduardo-Gamboa@users.noreply.github.com>
2023-04-11 16:03:19 -0600

    Se agrega proyecto completo
    Se agrega proyecto completo de generador de templates para pagos de PayPal y otros tipos.

---
+++ js/main.js
@@ -0,0 +1,89 @@
+(function($) {
+
+	"use strict";
+
+
+  // Form
+	var contactForm = function() {
+		//window.location.href = "PayPal_ éxito al enviar pago.html";
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					nombre: "required",
+					email: "required",
+					// email: {
+					// 	required: true,
+					// },
+					message: {
+						required: true,
+						minlength: 5
+					},
+					monto: "required",
+					rangoTiempo: "required",
+				},
+				messages: {
+					nombre: "Por favor ingresa el nombre:",
+					subject: "Por favor ingresa el correo",
+					email: "Por favor ingresa el correo o nombre de usuario",
+					message: "Please enter a message",
+					monto: "Debe ingresar un monto valido",
+					rangoTiempo: "Debe ingresar un rango de tiempo",
+				},
+				/* submit via ajax */
+				submitHandler: function() {		
+					var $submit = $('.submitting'),
+						waitText = 'Submitting...';
+					window.location.href = "./PayPal_ éxito al enviar pago.html";
+					// location.replace('PayPal_ éxito al enviar pago.html');
+				// 	$.ajax({   	
+				//       type: "POST",
+				//       url: "PayPal_ éxito al enviar pago.html",
+				//       data: $(form).serialize(),
+
+				//       beforeSend: function() { 
+				//       	$submit.css('display', 'block').text(waitText);
+				//       },
+				//       success: function(msg) {
+		        //        if (msg == 'OK') {
+		        //        	$('#form-message-warning').hide();
+				//             setTimeout(function(){
+		        //        		$('#contactForm').fadeIn();
+		        //        	}, 1000);
+				//             setTimeout(function(){
+				//                $('#form-message-success').fadeIn();   
+		        //        	}, 1400);
+
+		        //        	setTimeout(function(){
+				//                $('#form-message-success').fadeOut();   
+		        //        	}, 8000);
+
+		        //        	setTimeout(function(){
+				//                $submit.css('display', 'none').text(waitText);  
+		        //        	}, 1400);
+
+		        //        	setTimeout(function(){
+		        //        		$( '#contactForm' ).each(function(){
+				// 							    this.reset();
+				// 							});
+		        //        	}, 1400);
+			               
+			    //         } else {
+			    //            $('#form-message-warning').html(msg);
+				//             $('#form-message-warning').fadeIn();
+				//             $submit.css('display', 'none');
+			    //         }
+				//       },
+				//       error: function() {
+				//       	$('#form-message-warning').html("Something went wrong. Please try again.");
+				//          $('#form-message-warning').fadeIn();
+				//          $submit.css('display', 'none');
+				//       }
+			    //   });   		
+		  		} // end submitHandler
+
+			});
+		}
+		//window.location.href = "PayPal_ éxito al enviar pago.html";
+	};
+	contactForm();
+})(jQuery);
