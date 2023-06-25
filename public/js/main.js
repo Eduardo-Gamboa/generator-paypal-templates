9b47bbecfaeb121b2505967e9d123929b9bed7b6
Eduardo Gamboa <69367792+Eduardo-Gamboa@users.noreply.github.com>
2023-06-25 01:48:57 -0600

    Nuevas modificaciones
    Se agrega selección de tipos de moneda internacional y se corrigen detalles del template.

--- public/js/main.js
+++ public/js/main.js
@@ -1,53 +1,54 @@
 (function($) {
 
-	"use strict";
+    "use strict";
 
-	var nombre = document.getElementById("nombre");
-	var emailUsername = document.getElementById("email");
-	var monto = document.getElementById("monto");
-	var rangoTiempo = document.getElementById("rangoTiempo");
-  // Form
-	var contactForm = function() {
-		//window.location.href = "PayPal_ éxito al enviar pago.html";
-		if ($('#contactForm').length > 0 ) {
-			$( "#contactForm" ).validate( {
-				rules: {
-					// nombre: "required",
-					email: "required",
-					// email: {
-					// 	required: true,
-					// },
-					message: {
-						required: true,
-						minlength: 5
-					},
-					monto: "required",
-					rangoTiempo: "required",
-				},
-				messages: {
-					nombre: "Por favor ingresa el nombre:",
-					subject: "Por favor ingresa el correo",
-					email: "Por favor ingresa el correo o nombre de usuario",
-					message: "Please enter a message",
-					monto: "Debe ingresar un monto valido",
-					rangoTiempo: "Debe ingresar un rango de tiempo",
-				},
-				/* submit via ajax */
-				submitHandler: function() {		
-					var $submit = $('.submitting'),
-						waitText = 'Submitting...';
+    var nombre = document.getElementById("nombre");
+    var emailUsername = document.getElementById("email");
+    var monto = document.getElementById("monto");
+    var rangoTiempo = document.getElementById("rangoTiempo");
+    var moneda = document.getElementById("cmbMoneda");
+    // Form
+    var contactForm = function() {
+        //window.location.href = "PayPal_ éxito al enviar pago.html";
+        if ($('#contactForm').length > 0) {
+            $("#contactForm").validate({
+                rules: {
+                    // nombre: "required",
+                    email: "required",
+                    // email: {
+                    // 	required: true,
+                    // },
+                    message: {
+                        required: true,
+                        minlength: 5
+                    },
+                    monto: "required",
+                    rangoTiempo: "required",
+                },
+                messages: {
+                    nombre: "Por favor ingresa el nombre:",
+                    subject: "Por favor ingresa el correo",
+                    email: "Por favor ingresa el correo o nombre de usuario",
+                    message: "Please enter a message",
+                    monto: "Debe ingresar un monto valido",
+                    rangoTiempo: "Debe ingresar un rango de tiempo",
+                },
+                /* submit via ajax */
+                submitHandler: function() {
+                        var $submit = $('.submitting'),
+                            waitText = 'Submitting...';
 
-					window.open("PaySend.html?nombre="+nombre.value+"&emailUsername="+emailUsername.value+"&monto="+monto.value+"&rangoTiempo="+rangoTiempo.value+"", "_blank");
-		  		} // end submitHandler
-			});
-		}
-	};
-	contactForm();
+                        window.open("PaySend.html?nombre=" + nombre.value + "&emailUsername=" + emailUsername.value + "&monto=" + monto.value + "&rangoTiempo=" + rangoTiempo.value + "&moneda=" + moneda.value + "", "_blank");
+                    } // end submitHandler
+            });
+        }
+    };
+    contactForm();
 })(jQuery);
 
 function limpiarCampos() {
-	nombre.value = "";
-	//emailUsername.value = "";
-	monto.value = "";
-	rangoTiempo.value = "";
+    nombre.value = "";
+    //emailUsername.value = "";
+    monto.value = "";
+    rangoTiempo.value = "";
 }
--- public/js/pagoEnviado.js
+++ public/js/pagoEnviado.js
@@ -25,23 +25,23 @@
 
 (function($) {
 
-	"use strict";
+    "use strict";
 
-	var Init = function() {
+    var Init = function() {
         var txtNombre = document.getElementById("txtNombre");
         var txtMonto = document.getElementById("txtMonto");
         var txtEmailEnviado = document.getElementById("txtEmailEnviado");
         var txtRangoTiempo = document.getElementById("txtRangoTiempo");
-
+        var txtMoneda = document.getElementById("txtMoneda");
+        // debugger;
         var queryString = window.location.search;
-
+        // var moneda = txtMoneda.innerHTML;
         // Elimina el signo de interrogación al principio de la cadena de consulta
         queryString = queryString.slice(1);
 
         // Divide la cadena de consulta en pares de nombre y valor separados por "&"
         var params = queryString.split("&");
 
-        debugger;
         // Crea un objeto para almacenar los parámetros y sus valores
         var queryParams = {};
         for (var i = 0; i < params.length; i++) {
@@ -51,18 +51,20 @@
             queryParams[name] = value;
         }
         // Accede al valor del parámetro "nombre"
-        if(queryParams.nombre === "" || queryParams.nombre === undefined || queryParams.nombre === null){
+        if (queryParams.nombre === "" || queryParams.nombre === undefined || queryParams.nombre === null) {
             txtNombre.innerHTML = queryParams.emailUsername;
-        }else{
+        } else {
             txtNombre.innerHTML = queryParams.nombre;
         }
         txtMonto.innerHTML = queryParams.monto;
         txtEmailEnviado.innerHTML = queryParams.emailUsername;
         txtRangoTiempo.innerHTML = queryParams.rangoTiempo;
         txtEmailUserVer.innerHTML = queryParams.emailUsername;
+        txtMoneda.innerHTML = queryParams.moneda;
+        // txtMoneda.innerHTML
         // var sNombre = queryParams.nombre;
         // var sNombre = queryParams.nombre;
         // var sNombre = queryParams.nombre;
-	};
-	Init();
+    };
+    Init();
 })(jQuery);
