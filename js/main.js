60c64a1e63621998efbc9b4712dfa6e8cc2ed62b
EduardoGamboa <desarrollo2@turistore.com>
2023-11-05 12:45:46 -0600

    se agregan nuevos cambios al repositorio

--- js/main.js
+++ js/main.js
@@ -2,45 +2,89 @@
 
     "use strict";
 
+    // Variables
     var nombre = document.getElementById("nombre");
     var emailUsername = document.getElementById("email");
     var monto = document.getElementById("monto");
     var rangoTiempo = document.getElementById("rangoTiempo");
     var moneda = document.getElementById("cmbMoneda");
+    var tipoTemplate = document.getElementById("cmbTipoTemplate");
+    cambiarSeleccion();
     // Form
     var contactForm = function() {
+        debugger;
         //window.location.href = "PayPal_ éxito al enviar pago.html";
-        if ($('#contactForm').length > 0) {
-            $("#contactForm").validate({
-                rules: {
-                    // nombre: "required",
-                    email: "required",
-                    // email: {
-                    // 	required: true,
-                    // },
-                    message: {
-                        required: true,
-                        minlength: 5
-                    },
-                    monto: "required",
-                    rangoTiempo: "required",
-                },
-                messages: {
-                    nombre: "Por favor ingresa el nombre:",
-                    subject: "Por favor ingresa el correo",
-                    email: "Por favor ingresa el correo o nombre de usuario",
-                    message: "Please enter a message",
-                    monto: "Debe ingresar un monto valido",
-                    rangoTiempo: "Debe ingresar un rango de tiempo",
-                },
-                /* submit via ajax */
-                submitHandler: function() {
-                        var $submit = $('.submitting'),
-                            waitText = 'Submitting...';
+        switch (tipoTemplate.value) {
+            case "P":
+                if ($('#fPayPal').length > 0) {
+                    $("#fPayPal").validate({
+                        rules: {
+                            // nombre: "required",
+                            email: "required",
+                            // email: {
+                            // 	required: true,
+                            // },
+                            message: {
+                                required: true,
+                                minlength: 5
+                            },
+                            monto: "required",
+                            rangoTiempo: "required",
+                        },
+                        messages: {
+                            nombre: "Por favor ingresa el nombre:",
+                            subject: "Por favor ingresa el correo",
+                            email: "Por favor ingresa el correo o nombre de usuario",
+                            message: "Please enter a message",
+                            monto: "Debe ingresar un monto valido",
+                            rangoTiempo: "Debe ingresar un rango de tiempo",
+                        },
+                        /* submit via ajax */
+                        submitHandler: function() {
+                                var $submit = $('.submitting'),
+                                    waitText = 'Submitting...';
 
-                        window.open("PaySend.html?nombre=" + nombre.value + "&emailUsername=" + emailUsername.value + "&monto=" + monto.value + "&rangoTiempo=" + rangoTiempo.value + "&moneda=" + moneda.value + "", "_blank");
-                    } // end submitHandler
-            });
+                                window.open("PaySend.html?nombre=" + nombre.value + "&emailUsername=" + emailUsername.value + "&monto=" + monto.value + "&rangoTiempo=" + rangoTiempo.value + "&moneda=" + moneda.value + "", "_blank");
+                            } // end submitHandler
+                    });
+                }
+                break;
+            case "M":
+                if ($('#fMercadoPago').length > 0) {
+                    $("#fMercadoPago").validate({
+                        rules: {
+                            // nombre: "required",
+                            email: "required",
+                            // email: {
+                            // 	required: true,
+                            // },
+                            message: {
+                                required: true,
+                                minlength: 5
+                            },
+                            monto: "required",
+                            rangoTiempo: "required",
+                        },
+                        messages: {
+                            nombre: "Por favor ingresa el nombre:",
+                            subject: "Por favor ingresa el correo",
+                            email: "Por favor ingresa el correo o nombre de usuario",
+                            message: "Please enter a message",
+                            monto: "Debe ingresar un monto valido",
+                            rangoTiempo: "Debe ingresar un rango de tiempo",
+                        },
+                        /* submit via ajax */
+                        submitHandler: function() {
+                                var $submit = $('.submitting'),
+                                    waitText = 'Submitting...';
+
+                                window.open("PaySend.html?nombre=" + nombre.value + "&emailUsername=" + emailUsername.value + "&monto=" + monto.value + "&rangoTiempo=" + rangoTiempo.value + "&moneda=" + moneda.value + "", "_blank");
+                            } // end submitHandler
+                    });
+                }
+                break;
+            default:
+                break;
         }
     };
     contactForm();
@@ -51,4 +95,25 @@ function limpiarCampos() {
     //emailUsername.value = "";
     monto.value = "";
     rangoTiempo.value = "";
+}
+
+function enviarDatos() {
+    window.open("Detalle Transferencia MP - Movil.html");
+}
+
+$("#cmbTipoTemplate").change(function() {
+    cambiarSeleccion();
+});
+
+function cambiarSeleccion() {
+    var v = $("#cmbTipoTemplate").val();
+    if (v === "P") {
+        $("#hTipoSeleccion").text("PayPal");
+        $("#fPayPal").show();
+        $("#fMercadoPago").hide();
+    } else if (v === "M") {
+        $("#fPayPal").hide();
+        $("#fMercadoPago").show();
+        $("#hTipoSeleccion").text("Mercado Pago");
+    }
 }
---
+++ js/pagoEnviadoMP.js
@@ -0,0 +1,76 @@
+(function($) {
+
+    "use strict";
+
+    var Init = function() {
+        // Arreglos de nombres de meses y nombres de AM/PM
+        var meses = [
+            "enero", "febrero", "marzo", "abril", "mayo", "junio",
+            "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
+        ];
+
+        var amPm = ["AM", "PM"];
+
+        // Obtener la fecha y hora actual
+        var fechaActual = new Date();
+
+        // Obtener los componentes de la fecha y hora
+        var dia = fechaActual.getDate();
+        var mes = meses[fechaActual.getMonth()];
+        var horas = fechaActual.getHours();
+        var minutos = fechaActual.getMinutes();
+        var amOpm = amPm[horas >= 12 ? 1 : 0];
+
+        // Formatear los minutos para que tengan siempre 2 dígitos
+        if (minutos < 10) {
+            minutos = "0" + minutos;
+        }
+
+        // Construir la cadena de fecha en el formato deseado
+        var fechaFormateada = dia + " de " + mes + " " + horas + ":" + minutos + " " + amOpm;
+
+        // Mostrar la fecha formateada en el elemento HTML
+        // $("#fecha").text(fechaFormateada);
+
+
+        var txtNombre = document.getElementById("txtNombreUsuario");
+        var txtMonto = document.getElementById("txtMonto");
+        // var txtEmailEnviado = document.getElementById("txtEmailEnviado");
+        var txtRangoTiempo = document.getElementById("txtRangoTiempo");
+        var txtConcepto = document.getElementById("txtConceptoMP");
+        // var txtMoneda = document.getElementById("txtMoneda");
+        // debugger;
+        var queryString = window.location.search;
+        // var moneda = txtMoneda.innerHTML;
+        // Elimina el signo de interrogación al principio de la cadena de consulta
+        queryString = queryString.slice(1);
+
+        // Divide la cadena de consulta en pares de nombre y valor separados por "&"
+        var params = queryString.split("&");
+
+        // Crea un objeto para almacenar los parámetros y sus valores
+        var queryParams = {};
+        for (var i = 0; i < params.length; i++) {
+            var parts = params[i].split("=");
+            var name = decodeURIComponent(parts[0]);
+            var value = decodeURIComponent(parts[1]);
+            queryParams[name] = value;
+        }
+        // Accede al valor del parámetro "nombre"
+        // if (queryParams.nombre === "" || queryParams.nombre === undefined || queryParams.nombre === null) {
+        //     txtNombre.innerHTML = queryParams.emailUsername;
+        // } else {
+        //     txtNombre.innerHTML = queryParams.nombre;
+        // }
+        txtMonto.innerHTML = queryParams.monto;
+        txtNombre.innerHTML = queryParams.emailUsername;
+        txtRangoTiempo.innerHTML = queryParams.rangoTiempo;
+        txtEmailUserVer.innerHTML = queryParams.emailUsername;
+        txtConcepto.innerHTML = queryParams.concepto;
+        // txtMoneda.innerHTML
+        // var sNombre = queryParams.nombre;
+        // var sNombre = queryParams.nombre;
+        // var sNombre = queryParams.nombre;
+    };
+    Init();
+})(jQuery);
